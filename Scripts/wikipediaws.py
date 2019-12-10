abbc40460af9a9ffbc69a01ccb2d67d45d9cb93f
hassan <hassanshaheen08@gmail.com>
2019-12-10 19:17:07 +0200

    Task 2 continued

--- Scripts/wikipediaws.py
+++ Scripts/wikipediaws.py
@@ -1,8 +1,32 @@
 import wikipedia
+import re 
 
 searchresults = wikipedia.search("plant")
 summarylist = []
+newSummary = []
+
+mainSentence = "This plant requires watering every morning Plants"
+mainSentence = mainSentence.replace("plant","")
+
+tokenizedSentence = re.sub("[^\w]", " ", mainSentence).split()
 
 for results in searchresults:
     summarylist.append(wikipedia.summary(results))
-print(re)
+
+for item in summarylist:
+    newSummary.append(item.replace("plant",""))
+
+tokenList = []
+for item in newSummary:
+    tokenList.append(re.sub("[^\w]", " ", item).split())
+
+def jaccard_similarity(list1, list2):
+    intersection = len(list(set(list1).intersection(list2)))
+    union = (len(list1) + len(list2)) - intersection
+    return float(intersection) / union
+
+
+print(jaccard_similarity(tokenList[0],tokenizedSentence))
+
+
+
